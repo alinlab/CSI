1f4d75a85b8dd9b23b66b18f775a7950dedf3b90
mojtaba nafez <mojtabanafez96@gmail.com>
2023-09-24 02:47:26 +0330

    update

--- common/train.py
+++ common/train.py
@@ -63,7 +63,7 @@ if P.multi_gpu:
 else:
     train_loader = DataLoader(train_set, shuffle=True, batch_size=P.batch_size, **kwargs)
     test_loader = DataLoader(test_set, shuffle=False, batch_size=P.test_batch_size, **kwargs)
-
+print("333333333", len(train_loader), len(train_set))
 if P.ood_dataset is None:
     if P.one_class_idx is not None:
         P.ood_dataset = list(range(P.n_superclasses))
