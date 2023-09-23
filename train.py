177aa278c5e3a80c11a953b1b6a0ccc4301a3080
mojtaba nafez <mojtabanafez96@gmail.com>
2023-09-23 22:22:30 +0330

    update 4

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ for epoch in range(start_epoch, P.epochs + 1):
     if (epoch % save_step == 0):
         save_states = model.state_dict()
         save_checkpoint(epoch, save_states, optimizer.state_dict(), logger.logdir)
-        '''
+        
         from evals.ood_pre import eval_ood_detection
         P.load_path = logger.logdir
         P.ood_layer = ("simclr", "shift")
@@ -49,7 +49,7 @@ for epoch in range(start_epoch, P.epochs + 1):
         with torch.no_grad():
             auroc_dict = eval_ood_detection(P, model, test_loader, ood_test_loader, P.ood_score,
                                         train_loader=train_loader, simclr_aug=simclr_aug)
-        '''
+        
 epoch += 1
 if P.multi_gpu:
     save_states = model.module.state_dict()
