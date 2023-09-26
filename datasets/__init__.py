53994a702a267da12327b77fdb81bf6dd89063e6
mojtaba nafez <mojtabanafez96@gmail.com>
2023-09-26 15:52:53 +0330

    update

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,2 +1,2 @@
-from datasets.datasets import get_dataset, get_superclass_list, get_subclass_dataset, get_exposure_dataloader, mvtecad_dataset
+from datasets.datasets import set_dataset_count, get_dataset, get_superclass_list, get_subclass_dataset, get_exposure_dataloader, mvtecad_dataset
 
