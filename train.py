763178731bbf6e0f2d14140e9618b4c6676d48af
mojtaba nafez <mojtabanafez96@gmail.com>
2023-09-29 19:13:33 +0330

    update

--- train.py
+++ train.py
@@ -36,8 +36,8 @@ for epoch in range(start_epoch, P.epochs + 1):
     kwargs['linear_optim'] = linear_optim
     kwargs['simclr_aug'] = simclr_aug
 
-    # if epoch > 50:
-    if True:
+    
+    if epoch > 7:
         for param in model.parameters():
             param.requires_grad = True
 
