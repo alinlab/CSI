b98471f2331719202141ea719900e44b7a5a7e32
mojtaba nafez <mojtabanafez96@gmail.com>
2023-10-01 16:44:27 +0330

    update

--- common/common.py
+++ common/common.py
@@ -45,8 +45,6 @@ def parse_args(default=False):
                         default=None, type=str)
     parser.add_argument('--error_step', help='Epoch steps to compute errors',
                         default=5, type=int)
-    parser.add_argument('--save_step', help='Epoch steps to save models',
-                        default=10, type=int)
 
     ##### Training Configurations #####
     parser.add_argument('--epochs', help='Epochs',
