74fbe201db93bbbafe1add8d498bf811849d0195
mojtaba nafez <mojtabanafez96@gmail.com>
2023-10-15 16:05:35 +0330

    update

--- models/resnet.py
+++ models/resnet.py
@@ -250,7 +250,8 @@ class Pretrain_ResNet152_Corruption(BaseModel):
 
         self.in_planes = 64
         self.last_dim = last_dim
-
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        
         mu = torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1).cuda()
         std = torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1).cuda()
         self.norm = lambda x: (x - mu) / std
