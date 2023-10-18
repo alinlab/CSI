115eaec92452269bf8ccd6c25fef6d9275a99bbb
Mohammad Jafari <mamad.jafari91@gmail.com>
2023-10-18 20:03:36 +0330

    fix supervised contrastive loss

--- training/sup/sup_simclr_CSI.py
+++ training/sup/sup_simclr_CSI.py
@@ -6,16 +6,17 @@ import models.transform_layers as TL
 from training.contrastive_loss import get_similarity_matrix, Supervised_NT_xent
 from utils.utils import AverageMeter, normalize
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device(f"cuda" if torch.cuda.is_available() else "cpu")
 hflip = TL.HorizontalFlipLayer().to(device)
 
 
-def train(P, epoch, model, criterion, optimizer, scheduler, loader, logger=None,
+def train(P, epoch, model, criterion, optimizer, scheduler, loader, train_exposure_loader=None, logger=None,
           simclr_aug=None, linear=None, linear_optim=None):
 
-    # currently only support rotation shifting augmentation
     assert simclr_aug is not None
-    assert P.sim_lambda == 1.0
+    assert P.sim_lambda == 1.0  # to avoid mistake
+    P.K_shift = 2
+    assert P.K_shift > 1
 
     if logger is None:
         log_ = print
@@ -28,9 +29,19 @@ def train(P, epoch, model, criterion, optimizer, scheduler, loader, logger=None,
     losses = dict()
     losses['cls'] = AverageMeter()
     losses['sim'] = AverageMeter()
+    losses['shift'] = AverageMeter()
 
     check = time.time()
+    train_exposure_loader_iterator = iter(train_exposure_loader)
+    print("len(train_exposure_loader_iterator), len(loader): ", len(train_exposure_loader_iterator), len(loader))
+
     for n, (images, labels) in enumerate(loader):
+        try:
+            exposure_images, _ = next(train_exposure_loader_iterator)
+        except StopIteration:
+            train_exposure_loader_iterator = iter(train_exposure_loader)
+            exposure_images, _ = next(train_exposure_loader_iterator)
+        # print(exposure_images.shape, images.shape, labels.shape)
         model.train()
         count = n * P.n_gpus  # number of trained samples
 
@@ -41,29 +52,35 @@ def train(P, epoch, model, criterion, optimizer, scheduler, loader, logger=None,
         if P.dataset != 'imagenet':
             batch_size = images.size(0)
             images = images.to(device)
+            exposure_images = exposure_images.to(device)
             images1, images2 = hflip(images.repeat(2, 1, 1, 1)).chunk(2)  # hflip
+            exposure_images1, exposure_images2 = hflip(exposure_images.repeat(2, 1, 1, 1)).chunk(2)  # hflip
         else:
             batch_size = images[0].size(0)
             images1, images2 = images[0].to(device), images[1].to(device)
-
-        images1 = torch.cat([torch.rot90(images1, rot, (2, 3)) for rot in range(4)])  # 4B
-        images2 = torch.cat([torch.rot90(images2, rot, (2, 3)) for rot in range(4)])  # 4B
-        images_pair = torch.cat([images1, images2], dim=0)  # 8B
-
         labels = labels.to(device)
-        rot_sim_labels = torch.cat([labels + P.n_classes * i for i in range(4)], dim=0)
-        rot_sim_labels = rot_sim_labels.to(device)
+        
+        images1 = torch.cat([images1, exposure_images1])
+        images2 = torch.cat([images2, exposure_images2])
+        #images1 = torch.cat([P.shift_trans(images1, k) for k in range(P.K_shift)])
+        #images2 = torch.cat([P.shift_trans(images2, k) for k in range(P.K_shift)])
+        #shift_labels = torch.cat([torch.ones_like(labels) * k for k in range(P.K_shift)], 0)  # B -> 4B
+        shift_labels = torch.cat([torch.ones_like(labels), torch.zeros_like(labels)], 0)  # B -> 4B
+        shift_labels = shift_labels.repeat(2)
+        
+        images_pair = torch.cat([images1, images2], dim=0)  # 8B
+        images_pair = simclr_aug(images_pair)  # transform
 
-        images_pair = simclr_aug(images_pair)  # simclr augment
-        _, outputs_aux = model(images_pair, simclr=True, penultimate=True)
+        _, outputs_aux = model(images_pair, simclr=True, penultimate=False, shift=True)
 
         simclr = normalize(outputs_aux['simclr'])  # normalize
         sim_matrix = get_similarity_matrix(simclr, multi_gpu=P.multi_gpu)
-        loss_sim = Supervised_NT_xent(sim_matrix, labels=rot_sim_labels,
-                                      temperature=0.07, multi_gpu=P.multi_gpu) * P.sim_lambda
+        loss_sim = Supervised_NT_xent(sim_matrix, labels, temperature=0.5) * P.sim_lambda
+
+        loss_shift = criterion(outputs_aux['shift'], shift_labels)
 
         ### total loss ###
-        loss = loss_sim
+        loss = loss_sim + loss_shift
 
         optimizer.zero_grad()
         loss.backward()
@@ -74,37 +91,23 @@ def train(P, epoch, model, criterion, optimizer, scheduler, loader, logger=None,
 
         batch_time.update(time.time() - check)
 
-        ### Post-processing stuffs ###
-        penul_1 = outputs_aux['penultimate'][:batch_size]
-        penul_2 = outputs_aux['penultimate'][4 * batch_size: 5 * batch_size]
-        outputs_aux['penultimate'] = torch.cat([penul_1, penul_2])  # only use original rotation
-
-        ### Linear evaluation ###
-        outputs_linear_eval = linear(outputs_aux['penultimate'].detach())
-        loss_linear = criterion(outputs_linear_eval, labels.repeat(2))
-
-        linear_optim.zero_grad()
-        loss_linear.backward()
-        linear_optim.step()
-
-        ### Log losses ###
         losses['cls'].update(0, batch_size)
         losses['sim'].update(loss_sim.item(), batch_size)
+        losses['shift'].update(loss_shift.item(), batch_size)
 
         if count % 50 == 0:
             log_('[Epoch %3d; %3d] [Time %.3f] [Data %.3f] [LR %.5f]\n'
-                 '[LossC %f] [LossSim %f]' %
+                 '[LossC %f] [LossSim %f] [LossShift %f]' %
                  (epoch, count, batch_time.value, data_time.value, lr,
-                  losses['cls'].value, losses['sim'].value))
+                  losses['cls'].value, losses['sim'].value, losses['shift'].value))
 
-        check = time.time()
-
-    log_('[DONE] [Time %.3f] [Data %.3f] [LossC %f] [LossSim %f]' %
+    log_('[DONE] [Time %.3f] [Data %.3f] [LossC %f] [LossSim %f] [LossShift %f]' %
          (batch_time.average, data_time.average,
-          losses['cls'].average, losses['sim'].average))
+          losses['cls'].average, losses['sim'].average, losses['shift'].average))
 
     if logger is not None:
         logger.scalar_summary('train/loss_cls', losses['cls'].average, epoch)
         logger.scalar_summary('train/loss_sim', losses['sim'].average, epoch)
+        logger.scalar_summary('train/loss_shift', losses['shift'].average, epoch)
         logger.scalar_summary('train/batch_time', batch_time.average, epoch)
 
